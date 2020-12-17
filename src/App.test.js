8ddbeb4757937bf37901a8e0e294be3ee6099a38
Jinhua.Tu <jinhua.tu@a2baustralia.com>
2020-12-17 16:40:24 +1100

    Start testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,29 @@
 import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
 import App from './App';
 
-test('renders without error', () => {
+Enzyme.configure({ adapter: new Adapter() });
+
+test('renders without crashing', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
+});
+
+test('renders a button', () => {
+
+});
+
+test('renders counter display', () => {
   
-})
+});
+
+test('counter starts at 0', () => {
+
+});
+
+test('clicking on button increments counter display', () => {
+
+});
